761fc94267c2cfb2a4ea58aaed751f3e79834127
PHENOMVALENCE <phenomenalvalence@gmail.com>
2025-04-12 17:58:05 +0300

    fourth commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="icon" href="2.png" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proma Africa - Property Valuation & Real Estate Services</title>
